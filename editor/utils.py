ebe3f839c0b5c80e9967c37c748d7cf16a214bfd
Anubhav <choubey.anubhav253@gmail.com>
2025-07-01 22:57:56 +0530

    Giving Good heading right now

--- editor/utils.py
+++ editor/utils.py
@@ -1,20 +1,24 @@
 import os
-from moviepy import AudioFileClip, TextClip, CompositeVideoClip, VideoFileClip
+from moviepy import AudioFileClip, TextClip, CompositeVideoClip, VideoFileClip, vfx
 
-def add_heading(video : VideoFileClip , heading : str) -> CompositeVideoClip:
+def add_heading(video: VideoFileClip, heading: str) -> CompositeVideoClip:
     text_clip = TextClip(
-            font=r"static/Utendo-Bold.ttf",
-            text=heading,
-            font_size=80,
-            color='black',
-            size=video.size,
-            duration=video.duration,
-            text_align="center",
-            margin=(None, 50),
-            stroke_color="white",
-            stroke_width=8,
-            vertical_align="top",
-        ).with_start(0)
-    # Removed the incorrect line
+        text=heading,
+        font=r"static/Utendo-Bold.ttf",
+        font_size=57,
+        color='white',
+        bg_color='black',
+        size=(video.size[0], None),
+        duration=video.duration,
+        text_align="center",
+        margin=(None, 25),
+        method="caption",
+        interline=25,
+        vertical_align="top",
+    ).with_start(0)
+
+    fade_duration = min(1, video.duration / 5)
+    text_clip = text_clip.with_effects([vfx.SlideIn(fade_duration, 'top'), vfx.SlideOut(fade_duration, 'top')])
+
     video_with_text = CompositeVideoClip([video, text_clip])
     return video_with_text
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from moviepy import VideoFileClip
 
 video_path = "testing_stuff/vid_1.mp4"
 video = VideoFileClip(video_path)
-heading = "Coding Till 20LPA\n Day 8"
+heading = "Coding Till 20LPA Day 8"
 video_with_heading = add_heading(video, heading)
 output_path = "testing_stuff/vid_1_with_heading.mp4"
 video_with_heading.write_videofile(output_path, codec="h264", audio_codec="aac", bitrate="3000k", fps=30)  # Adjust bitrate as needed
