352023329480c4958c83b49a3c5530a0e654db15
Anubhav <choubey.anubhav253@gmail.com>
2025-07-01 22:07:53 +0530

    Heading is working correcttly

--- editor/utils.py
+++ editor/utils.py
@@ -3,16 +3,16 @@ from moviepy import AudioFileClip, TextClip, CompositeVideoClip, VideoFileClip
 
 def add_heading(video : VideoFileClip , heading : str) -> CompositeVideoClip:
     text_clip = TextClip(
-            font=r"static/Utendo-Regular.ttf",
+            font=r"static/Utendo-Bold.ttf",
             text=heading,
-            font_size=90,
+            font_size=80,
             color='black',
             size=video.size,
             duration=video.duration,
             text_align="center",
             margin=(None, 50),
             stroke_color="white",
-            stroke_width=10,
+            stroke_width=8,
             vertical_align="top",
         ).with_start(0)
     # Removed the incorrect line
