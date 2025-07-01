6dc439eb1ac908e759fed2b45d2d067d82f97d8d
Anubhav <choubey.anubhav253@gmail.com>
2025-07-01 21:26:30 +0530

    Added structure to the repo

---
+++ editor/utils.py
@@ -0,0 +1,19 @@
+import os
+from moviepy import AudioFileClip, TextClip, CompositeVideoClip, VideoFileClip
+
+def add_heading(video, heading):
+    text_clip = TextClip(
+            font=r"static/Utendo-Regular.ttf",
+            text=heading,
+            font_size=90,
+            color='black',
+            size=video.size,
+            duration=video.duration,
+            text_align="center",
+            margin=(None, 50),
+            stroke_color="white",
+            stroke_width=10,
+            vertical_align="top",
+        ).with_start(0)
+    video_with_text = CompositeVideoClip([video, text_clip])
+    return video_with_text
---
+++ test.py
@@ -0,0 +1,3 @@
+from editor.utils import add_heading
+
+video_path = "testing_stuff/vid_1.mp4"
