41e0933a4569e7335a4f3bcf37e0fc7fa1df0f8d
Anubhav <choubey.anubhav253@gmail.com>
2025-08-23 10:05:42 +0530

    Need to start from scratch

--- src/core/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Core components for Ultimate Shorts Editor
-"""
-
-from .video_project import VideoProject
-from .media_manager import MediaManager
-from .project_config import ProjectConfig
-
-__all__ = ['VideoProject', 'MediaManager', 'ProjectConfig']
--- src/ui/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-UI Components for Ultimate Shorts Editor
-"""
-
-from .main_window import MainWindow
-from .components import *
-
-__all__ = ['MainWindow']
